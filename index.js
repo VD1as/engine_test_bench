170935887f127f1953054aa8fca2def4c1eee168
Vinícius Dias <98835180+VD1as@users.noreply.github.com>
2025-05-29 22:39:02 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,49 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient } = require('mongodb');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const uri = "mongodb+srv://vdiasnoite:Vdcp15243%402530@cluster0.rw5dk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const client = new MongoClient(uri);
+
 app.use(cors());
 app.use(express.json());
 
-app.post('/data', (req, res) => {
-  console.log("📥 Dados recebidos:");
-  console.log(req.body); 
-  res.status(200).json({ message: "Dados recebidos com sucesso!" });
+let collection;
+
+async function startServer() {
+  try {
+    await client.connect();
+    const db = client.db("engine_workbench");
+    collection = db.collection("dados");
+    console.log("Conectado ao MongoDB Atlas");
+
+    app.listen(PORT, () => {
+      console.log(`Servidor http://localhost:${PORT}`);
+    });
+
+  } catch (err) {
+    console.error("Erro ao conectar no MongoDB:", err);
+  }
+}
+
+app.post('/data', async (req, res) => {
+  try {
+    if (!collection) {
+      return res.status(500).json({ error: "Banco de dados não está pronto ainda." });
+    }
+
+    const data = req.body;
+    const result = await collection.insertOne(data);
+
+    console.log("Dados salvos no MongoDB:", data);
+    res.status(200).json({ message: "Dados salvos com sucesso", id: result.insertedId });
+  } catch (err) {
+    console.error("Erro ao salvar no MongoDB:", err);
+    res.status(500).json({ error: "Erro ao salvar os dados" });
+  }
 });
 
-app.listen(PORT, () => {
-  console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
-});
+startServer();
